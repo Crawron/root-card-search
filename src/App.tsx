5a5ee80cc9a6de9e55ed554a3c7f5d537ee590a8
Crawron <azertyaaron@gmail.com>
2021-09-05 02:06:22 -0500

    a lot

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
+import Fuse from "fuse.js"
+import clsx from "clsx"
 import { Card } from "./Card"
 import { cardList } from "./cardList"
-import Fuse from "fuse.js"
 import "./index.css"
+import { Logo } from "./Logo"
+import { IconMoon, IconSun } from "./Icons"
 
 const fuse = new Fuse(cardList, {
 	keys: ["name", "description", "type"],
 	ignoreLocation: true,
 })
 
+const buttonStyle = clsx`grid place-items-center font-bold h-9 px-2 rounded transition-colors border border-black/10 hover:bg-black/20 active:bg-black/50 dark:border-white/10 dark:hover:bg-white/20 dark:active:bg-black`
+
 function App() {
+	const [darkTheme, setDarkTheme] = useState(false)
 	const [nameFilter, setNameFilter] = useState("")
+
+	useEffect(() => {
+		if (darkTheme) document.documentElement.classList.add("dark")
+		else document.documentElement.classList.remove("dark")
+	}, [darkTheme])
 
 	const renderCard = (card: Card) => (
 		<CardCard key={card.suit + card.name} {...card} />
@@ -22,14 +33,28 @@ function App() {
 		.map(renderCard)
 
 	return (
-		<div className="App p-4">
-			<input
-				type="text"
-				className="border rounded p-1"
-				value={nameFilter}
-				onChange={(v) => setNameFilter(v.target.value)}
-			/>
-			<div className="flex flex-wrap gap-2">
+		<div className="App p-4 py-16 w-full max-w-3xl mx-auto">
+			<div>
+				<div className="flex flex-wrap gap-2 items-end">
+					<Logo />
+					<span className="-mb-1 font-bold mr-8">Card Search</span>
+					<input
+						type="text"
+						className="border flex-1 rounded p-1"
+						value={nameFilter}
+						onChange={(v) => setNameFilter(v.target.value)}
+					/>
+				</div>
+				<div className="flex gap-2">
+					<button
+						className={buttonStyle}
+						onClick={() => setDarkTheme(!darkTheme)}
+					>
+						{darkTheme ? <IconSun /> : <IconMoon />}
+					</button>
+				</div>
+			</div>
+			<div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(11rem,1fr))]">
 				{filteredList.length ? filteredList : cardList.map(renderCard)}
 			</div>
 		</div>
@@ -40,15 +65,15 @@ function App() {
 function CardCard(props: Card) {
 	const { name, description, amount, suit, assetPath, craftingPieces } = props
 	return (
-		<div className="w-52">
+		<div className="transition-all cursor-pointer transform hover:-translate-y-1 active:translate-y-0 active:brightness-75">
 			{/* <div className="flex px-4 gap-4">
 				<span>{suit}</span>
 				<div className="flex-1" />
 				<span>{craftingPieces?.map(([c]) => c).join(" ")}</span>
 			</div> */}
-			<div
-				className="rounded text-white shadow w-full h-72 p-4 bg-cover"
-				style={{ backgroundImage: `url('/${assetPath}.png')` }}
+			<img
+				className="transition-shadow rounded shadow hover:shadow-md w-full overflow-hidden"
+				src={`/${assetPath}.png`}
 			/>
 			{/* <div className="flex gap-2">
 				<span className="font-semibold flex-1">{name}</span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
 	mode: "jit",
 	purge: ["./index.html", "./src/**/*.{ts,tsx}"],
-	darkMode: false, // or 'media' or 'class'
+	darkMode: "class", // or 'media' or 'class'
 	theme: {
 		extend: {
+			colors: {
+				"fox-light": "#D83A30",
+				"rabbit-light": "#D9A900",
+				"mouse-light": "#DC7441",
+				"bird-light": "#36B0AE",
+
+				"fox-dark": "#FF6666",
+				"rabbit-dark": "#FBE545",
+				"mouse-dark": "#F68B57",
+				"bird-dark": "#5EC3C1",
+
+				"warm-gray": colors.warmGray,
+			},
 			fontFamily: {
 				serif: "Libre Baskerville",
 			},
