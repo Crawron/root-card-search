22a30fbc753a2baf4691127bbfeb001591377a75
Crawron <azertyaaron@gmail.com>
2021-09-05 19:47:58 -0500

    Prevent card dragging

--- src/components/CardCard.tsx
+++ src/components/CardCard.tsx
@@ -4,8 +4,12 @@ import { Card } from "../Card"
 // :)
 export function CardCard(props: Card) {
 	const { name, description, amount, suit, assetPath, craftingPieces } = props
+
 	return (
-		<div className="transition-all cursor-pointer select-none transform hover:-translate-y-1 active:translate-y-0 active:brightness-75">
+		<div
+			className="transition-all cursor-pointer transform hover:-translate-y-1 active:translate-y-0 active:brightness-75"
+			draggable="false"
+		>
 			{/* <div className="flex px-4 gap-4">
                 <span>{suit}</span>
                 <div className="flex-1" />
@@ -14,6 +18,7 @@ export function CardCard(props: Card) {
 			<img
 				loading="lazy"
 				className="transition-shadow rounded shadow hover:shadow-md w-full overflow-hidden"
+				onDragStart={(event) => event.preventDefault()}
 				src={`/${assetPath}.png`}
 			/>
 			{/* <div className="flex gap-2">
