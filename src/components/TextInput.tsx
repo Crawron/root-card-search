75906d4720639ccd0eef712796561a0931b8285f
Crawron <azertyaaron@gmail.com>
2021-09-05 20:04:47 -0500

    text input focus state

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -8,13 +8,13 @@ export function TextInput(props: {
 }) {
 	const { value, onChange, placeholder } = props
 	return (
-		<div className="flex flex-1 -mb-2.5 h-9 overflow-hidden place-items-center border-b-2 border-current min-w-min">
+		<div className="flex flex-1 transition-colors -mb-2.5 h-9 overflow-hidden place-items-center border-b-2 border-current min-w-min dark:focus-within:bg-yellow-100/20 focus-within:bg-yellow-900/20 rounded-sm">
 			<div className="px-1.5">
 				<IconSearch />
 			</div>
 			<input
 				type="text"
-				className="transition-colors flex-1 h-full outline-none bg-yellow-100 dark:bg-warm-gray-700 placeholder-shown:italic placeholder-black/40 dark:placeholder-yellow-100/50"
+				className="transition-colors flex-1 h-full leading-none outline-none bg-transparent placeholder-shown:italic placeholder-black/40 dark:placeholder-yellow-100/50"
 				value={value}
 				placeholder={placeholder}
 				onChange={(v) => onChange(v.target.value)}
